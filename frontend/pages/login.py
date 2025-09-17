ecc8ab489ede3da331262329ab140822dc7ba112
StillDoge312 <Anonymous055755@gmail.com>
2025-09-17 20:47:49 +0500

    Improve login and registration button styling
    Refactored the login page to enhance the appearance of the registration button with new props and classes for a more modern look. Also improved code formatting for better readability.

--- frontend/pages/login.py
+++ frontend/pages/login.py
@@ -4,11 +4,15 @@ from backend.auth import verify_user
 
 @ui.page("/")
 def login_page():
-    with ui.card().classes("w-80 mx-auto mt-20 p-6 shadow-lg"):
+    with ui.card().classes(
+        "w-80 mx-auto mt-20 p-6 shadow-lg"
+        ):
         ui.label("Вход").classes("text-2xl mb-4")
 
         username_input = ui.input("Логин").classes("w-full mb-2")
-        password_input = ui.input("Пароль", password=True, password_toggle_button=True).classes("w-full mb-2")
+        password_input = ui.input("Пароль", password=True, password_toggle_button=True).classes(
+            "w-full mb-2"
+            )
 
         def try_login():
             username = (username_input.value or "").strip()
@@ -20,11 +24,11 @@ def login_page():
             ok, res = verify_user(username, password)
             if ok:
                 ui.notify("Успешный вход", color="positive")
-                # Логин пройден — переходим в профиль.
-                # Пока сессии нет, просто переход.
                 ui.navigate.to("/profile")
             else:
                 ui.notify(f"Ошибка: {res}", color="negative")
 
         ui.button("Войти", on_click=try_login).classes("w-full mb-2")
-        ui.button("Регистрация", on_click=lambda: ui.navigate.to("/register")).classes("w-full outlined")
+        ui.button("Регистрация",on_click=lambda: ui.navigate.to("/register")).props("flat unelevated no-caps").classes(
+        "mx-auto bg-transparent shadow-none border-none text-blue-600 hover:underline"
+        )
